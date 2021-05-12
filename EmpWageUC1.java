e5eaa476c7a9ea487d74d602e621095adf73f22f
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-12 10:39:27 +0530

    add UC1 empwage program

---
+++ EmpWageUC1.java
@@ -0,0 +1,12 @@
+package com.empwage;
+
+public class EmpWageUC1 {
+	public static void main(String[] args) {
+		int Is_Full_Time = 1 ;
+	   double empCheck = Math.floor(Math.random()*10) % 2;
+	   if (empCheck == Is_Full_Time)
+		System.out.println("Employee is Present");
+	   else
+		   System.out.println("Employee is Absent");
+	}
+}
